59b8c49cd97299f5cc3d9fb458ffda75859c21d7
Prakhar-Verma001 <69008001+Prakhar-Verma001@users.noreply.github.com>
2023-12-22 16:25:51 +0530

    Update Notes.js

--- src/Copmonents/Notes.js
+++ src/Copmonents/Notes.js
@@ -10,7 +10,7 @@ const Notes = (props) => {
 
   return (
     <>
-        <Paper elevation={3} sx={{height:'fit-content',borderRadius:4, p:2, m:2, position:'relative', maxWidth:'300px', pb:3}}>
+        <Paper elevation={5} sx={{height:'fit-content',borderRadius:4, p:2, m:2, position:'relative', maxWidth:'300px', pb:3}}>
             
             <Typography mt={1} variant='h6' fontSize={'1.2rem'} fontWeight={'600'}>{props.heading}</Typography>
             <Typography lineHeight={'1.4rem'} fontSize={'1.1rem'} variant='subtitle1'>{props.notes}</Typography>
